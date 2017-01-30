21f264ba4166d83137ba4bad943481b254c66a1b
Shantanu Chakrawarty <51011922+shantanuuchak@users.noreply.github.com>
2017-01-30 04:18:09 +0000

    targeting the DOM

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,5 @@
-console.log("Script loaded");
+// DOM Targetting
+const count = document.querySelector(".count");
+const btnAdd = document.querySelector(".btn-add");
+const btnSave = document.querySelector(".btn-save");
+const btnReset = document.querySelector(".btn-reset");
