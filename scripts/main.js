aa4538e10bc40ac25e3461c51b35c5e69e53cca0
Shantanu Chakrawarty <51011922+shantanuuchak@users.noreply.github.com>
2017-02-02 14:03:54 +0000

    how to count?

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,7 @@
 // DOM Targetting
-const count = document.querySelector(".count");
+const countPara = document.querySelector(".count");
 const btnAdd = document.querySelector(".btn-add");
 const btnSave = document.querySelector(".btn-save");
 const btnReset = document.querySelector(".btn-reset");
+
+let count = 0;
