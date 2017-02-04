8b3b8173e1c6dee85cdec96940d64d776a40e72f
Shantanu Chakrawarty <51011922+shantanuuchak@users.noreply.github.com>
2017-02-04 11:50:47 +0000

    count one rep

--- scripts/main.js
+++ scripts/main.js
@@ -4,4 +4,12 @@ const btnAdd = document.querySelector(".btn-add");
 const btnSave = document.querySelector(".btn-save");
 const btnReset = document.querySelector(".btn-reset");
 
+// Variables
 let count = 0;
+
+// Functions
+function addOne() {
+  count += 1;
+}
+
+
