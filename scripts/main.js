b46fed9d8c236ed1c48be8a7f11017258b989538
Shantanu Chakrawarty <51011922+shantanuuchak@users.noreply.github.com>
2017-02-05 18:51:31 +0000

    more meaningful names
    actually I updated the counter to rep and create a new log to keep a track of all the rep logs

--- scripts/main.js
+++ scripts/main.js
@@ -5,11 +5,16 @@ const btnSave = document.querySelector(".btn-save");
 const btnReset = document.querySelector(".btn-reset");
 
 // Variables
-let count = 0;
+let rep = 0;
+const repLog = [];
 
 // Functions
 function addOne() {
-  count += 1;
+  rep += 1;
 }
 
+function save() {}
 
+function reset() {
+  rep = 0;
+}
