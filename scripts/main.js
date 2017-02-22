eac41c37a24c8330d344e9e7a4ad9f2b80fa8ed8
Shantanu Chakrawarty <51011922+shantanuuchak@users.noreply.github.com>
2017-02-22 15:50:10 +0000

    entries functionality DOM

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,7 @@ const countPara = document.querySelector(".count");
 const btnAdd = document.querySelector(".btn-add");
 const btnSave = document.querySelector(".btn-save");
 const btnReset = document.querySelector(".btn-reset");
+const entries = document.querySelector(".entries");
 
 // Variables
 let rep = 0;
@@ -11,7 +12,7 @@ const repLog = [];
 // Paint DOM
 function paint() {
   countPara.textContent = rep;
-  repLog.textContent = repLog.toString();
+  entries.textContent = 'Entries: ' + repLog.join(' - ');
 }
 
 // Functions
